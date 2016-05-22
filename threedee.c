97ea53bbb1d907ceb9cdf3a0f0bbc4b466ad519e
charlie <chuckiels2011@gmail.com>
2016-05-22 15:20:45 -0400

    YES! MATRICES WORK! TAKE THAT LINEAR ALGEBRA

--- threedee.c
+++ threedee.c
@@ -225,10 +225,4 @@ void calc_mvp(mat4x4 mvp, mat4x4 model, vec3 pos, vec3 dir, vec3 up, float fov, 
     mat4x4_look_at(t, (vec3){0, 30, 6}, (vec3){0, 0, 0}, (vec3){0, 1, 0});
     mat4x4_mul(m, proj, t);
     mat4x4_mul(m, m, model);
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++)
-            printf("%f ", m[j][i]);
-        printf("\n");
-    }
-    puts("---");
 }
