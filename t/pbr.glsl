5eadbded3620ac3b1f8abf978aa6d3c0b798333c
nc <chuckiels2011@gmail.com>
2016-07-27 15:29:11 -0400

    yay for cook-torrance!

--- t/pbr.glsl
+++ t/pbr.glsl
@@ -1,53 +1,57 @@
 #version 330
-// taken from wikipedia
+
+precision highp float; //set default precision in glsl es 2.0
 
 in vec2 v_texcoord;
 in vec3 v_normal;
 in vec3 v_binormal;
 in vec3 v_pos;
 
+vec3 lightPos = vec3(4.0, 3.0, -3.0);
+vec3 camPos = vec3(4.0, 3.0, -3.0);
 out vec4 result;
 
-const vec3 lightPos = vec3(1.0,1.0,1.0);
-const vec3 ambientColor = vec3(0.1, 0.0, 0.0);
-const vec3 diffuseColor = vec3(0.5, 0.0, 0.0);
-const vec3 specColor = vec3(1.0, 1.0, 1.0);
-const float shininess = 16.0;
-const float screenGamma = 2.2; // Assume the monitor is calibrated to the sRGB color space
-
-float lambert(vec3 lightDir, vec3 normal) {
-    return max(0.0, dot(lightDir, normal));
-}
-
-float ggx(float rough, float noh) {
+float ggx(float NdotH, float rough) {
     float m = rough * rough;
     float m2 = m * m;
-    float d = (noh * m2 - noh) * noh + 1;
+    float d = (NdotH * m2 - NdotH) * NdotH + 1; // 2 mad
     return m2 / (d*d);
 }
 
+float cookTorranceSpecular(
+        vec3 lightDirection,
+        vec3 viewDirection,
+        vec3 surfaceNormal,
+        float roughness,
+        float fresnel) {
+    float VdotN = max(dot(viewDirection, surfaceNormal), 0.0);
+    float LdotN = max(dot(lightDirection, surfaceNormal), 0.0);
+
+    //Half angle vector
+    vec3 H = normalize(lightDirection + viewDirection);
+
+    //Geometric term
+    float NdotH = max(dot(surfaceNormal, H), 0.0);
+    float VdotH = max(dot(viewDirection, H), 0.000001);
+    float LdotH = max(dot(lightDirection, H), 0.000001);
+    float G1 = (2.0 * NdotH * VdotN) / VdotH;
+    float G2 = (2.0 * NdotH * LdotN) / LdotH;
+    float G = min(1.0, min(G1, G2));
+
+    //Distribution term
+    float D = ggx(NdotH, roughness);
+
+    //Fresnel term
+    float F = pow(1.0 - VdotN, fresnel);
+
+    //Multiply terms and done
+    return  G * F * D / max(3.14159265 * VdotN * LdotN, 0.000001);
+}
+
 void main() {
-    vec3 normal = normalize(v_normal);
     vec3 lightDir = normalize(lightPos - v_pos);
-
-    float lambertian = max(dot(lightDir,normal), 0.0);
-    float specular = 0.0;
-
-    if(lambertian > 0.0) {
-
-        vec3 V = normalize(-v_pos);
-        vec3 L = normalize(lightPos - v_pos);
-        vec3 H = normalize(L + V);
-
-        float specAngle = max(dot(H, normal), 0.0);
-        specular = ggx(shininess, specAngle);
-    }
-    vec3 colorLinear = ambientColor +
-        lambertian * diffuseColor +
-        specular * specColor;
-    // apply gamma correction (assume ambientColor, diffuseColor and specColor
-    // have been linearized, i.e. have no gamma correction in them)
-    vec3 colorGammaCorrected = pow(colorLinear, vec3(1.0/screenGamma));
-    // use the gamma corrected color in the fragment
-    result = vec4(colorGammaCorrected, 1.0);
+    vec3 eyeDir = normalize(camPos - v_pos);
+    vec3 norm = normalize(-v_normal); // i flipped the normals accidentally
+    float power = cookTorranceSpecular(lightDir, eyeDir, norm, 0.7, 0.0);
+    result = vec4(power, power, power, 1.0);
 }
